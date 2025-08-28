3acb2f03c19dfa4206ce3d1df6eeafb136915008
Pablo Barrios <exepablo96@gmail.com>
2025-08-27 23:18:40 -0300

    add redirect

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,8 @@
+import { Redirect } from "expo-router";
 import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <View className="mt-5">
-        <Text className="text-3xl" style={{ fontFamily: "WorkSans-Black" }}>
-          Hola mundo
-        </Text>
-        <Text className="text-2xl font-work-medium">Hola mundo</Text>
-        <Text className="text-xl font-work-light">Hola mundo</Text>
-      </View>
-    </SafeAreaView>
-  );
+  return <Redirect href="/home" />;
 };
 
 export default App;
